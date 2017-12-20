1983f5c59bbb1e546b59558d9f9fa3021004fadc
Shevraar <sky050577@gmail.com>
2017-12-20 08:51:40 +0100

    Début du script dégolassss

---
+++ test_menu.ps1
@@ -0,0 +1,24 @@
+﻿#Allow the script execution
+Set-ExecutionPolicy Unrestricted
+
+$Domain = Read-Host -Prompt "Please input your domain name"
+$Username = Read-Host -Prompt "Please input your username"
+
+$User = Get-Credential -Credential $Username
+
+Write-Host "Welcome"$User.UserName
+
+function menu
+{
+    Write-Host "1 - Save the security environment"
+    Write-Host "2 - Display the security environment"
+    Write-Host "2 - Restoration of the security environment"
+    Write-Host "3 - Modify the security environment"
+    Write-Host "4 - Change authentification"
+    Write-Host "5 - Help"
+    Write-Host "6 - Quit"
+}
+cls
+menu
+
+$Main_menu_choice = Read-Host -Prompt "What do you want to do?"
