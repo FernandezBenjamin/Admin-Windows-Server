d6eceb89dc8594240dc3b238d0c910e097041cd3
Shevraar <sky050577@gmail.com>
2017-12-20 14:44:36 +0100

    modif du script principal pour les credentials

--- test_menu.ps1
+++ test_menu.ps1
@@ -1,24 +1,38 @@
 ﻿#Allow the script execution
 Set-ExecutionPolicy Unrestricted
 
+Import-Module ActiveDirectory
+
 $Domain = Read-Host -Prompt "Please input your domain name"
 $Username = Read-Host -Prompt "Please input your username"
 
-$User = Get-Credential -Credential $Username
+#vérifier si l'utilisateur existe dans l'AD (voir plus j'ai pas encore checké)
+Invoke-Command -FilePath ..\..\powershell\Test-UserCredentials.ps1 -ComputerName $env:COMPUTERNAME
 
-Write-Host "Welcome"$User.UserName
+#$User = Get-Credential -Credential $Username
 
-function menu
+
+function menu #Main menu
 {
-    Write-Host "1 - Save the security environment"
-    Write-Host "2 - Display the security environment"
-    Write-Host "2 - Restoration of the security environment"
-    Write-Host "3 - Modify the security environment"
-    Write-Host "4 - Change authentification"
+cls #clean the screen
+Write-Host "Domain :"$Domain
+Write-Host "User :"$User.UserName
+
+Write-Host "============================================="
+Write-Host "                  MENU"
+Write-Host "============================================="
+
+    Write-Host "1 - Save the right environment"
+    Write-Host "2 - Display the right environment"
+    Write-Host "2 - Restoration of the right environment"
+    Write-Host "3 - Modify the right environment"
+    Write-Host "4 - Change security environment"
     Write-Host "5 - Help"
     Write-Host "6 - Quit"
+
+
 }
-cls
+
 menu
 
 $Main_menu_choice = Read-Host -Prompt "What do you want to do?"
