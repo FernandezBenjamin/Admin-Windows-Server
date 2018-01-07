3cbb09a5ab7e50eb7cb5f88f59e5264cb003ae9c
Quentin <boulanger.quentinpro@gmail.com>
2018-01-07 21:42:15 +0100

    Avec les aides

--- main.ps1
+++ main.ps1
@@ -9,7 +9,7 @@ Set-ExecutionPolicy Unrestricted
 $Global:DOMAIN = $null
 $Global:USER = $null
 
-
+#============================# FUNCTIONS CREDENTIAL #============================#
 
 #Clear User Info Function
 Function ClearUserInfo{
@@ -139,254 +139,342 @@ function mess_error($error_mess,$help_mess){
     Write-Host "## HELP ## $help_mess`n" -ForegroundColor Yellow
 }
 
-#MAIN Menu
-Function menuMain{
+#============================# ALL FUNCTION MENU #============================#
 
+Function header($title){
+  Write-Host "`nDomain : $Global:DOMAIN"
+  Write-Host "User : $Global:USER`n"
+  Write-Host "======================================================================"
+  Write-Host "`t$title"
+  Write-Host "======================================================================"
+}
+
+Function footer{
+  Write-Host "======================================================================"
+}
+
+
+# Main menu
+Function menuMain{
     $choice = 1
+    $min = 1
+    $max = 7
     do{
         cls #clean the screen
-        if( $choice -lt 1 -or $choice -gt 7){
-           mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter a value between 1 and 7. Enter '6' for help"
+        if( $choice -lt $min -or $choice -gt $max){
+           mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter a value between $min and $max. Enter '$max' for help"
         }
-        Write-Host "`nDomain : $Global:DOMAIN"
-        Write-Host "User : $Global:USER`n"
-        Write-Host "============================================="
-        Write-Host "                  MENU"
-        Write-Host "=============================================`n"
+        header("MENU PRINCIPAL")
         Write-Host "`t1 - Save the right environment"
         Write-Host "`t2 - Display the right environment"
         Write-Host "`t3 - Restoration of the right environment"
-        Write-Host "`t4 - Modify the right environment"
+        Write-Host "`t4 - Edit an organization unity"
         Write-Host "`t5 - Change security environment"
         Write-Host "`t6 - Help"
         Write-Host "`t7 - Quit`n"
-        Write-Host "============================================="
+        footer
 
         $choice = Read-Host -Prompt "What do you want to do?"
 
-    }while($choice -lt 1 -or $choice -gt 7)
+    }while($choice -lt $min -or $choice -gt $max)
     return $choice;
 }
 
+# Backup Menu
 Function menuBackup{
   $choice = 0
+  $min = 0
+  $max = 3
   do{
     cls #clean the screen
     if( $choice -lt 0 -or $choice -gt 3){
        mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter a value between 0 and 3. Enter '3' for help"
     }
-    Write-Host "`nDomain : $Global:DOMAIN"
-    Write-Host "User : $Global:USER`n"
-    Write-Host "============================================="
-    Write-Host "         SAVE THE RIGHT ENVIRONMENT"
-    Write-Host "=============================================`n"
+    header("MENU : SAVE THE RIGHT ENVIRONMENT")
     Write-Host "Would you do a backup of your right environment ?"
     Write-Host "`t0 - Return"
     Write-Host "`t1 - Yes"
     Write-Host "`t2 - No"
     Write-Host "`t3 - Help"
-    Write-Host "============================================="
+    footer
     $choice = Read-Host -Prompt "What do you want to do?"
   }while($choice -lt 0 -or $choice -gt 3)
   return $choice;
 }
 
+# Display right menu
 Function menuDisplayRight{
   $choice = 1
+  $min = 0
+  $max = 2
   do{
     cls #clean the screen
     if( $choice -lt 0 -or $choice -gt 2){
        mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter a value between 0 and 3. Enter '3' for help"
     }
-    Write-Host "`nDomain : $Global:DOMAIN"
-    Write-Host "User : $Global:USER`n"
-    Write-Host "============================================="
-    Write-Host "              DISPLAY THE RIGHT"
-    Write-Host "=============================================`n"
+    headerMenu("MENU : DISPLAY THE RIGHT")
     Write-Host "`t0 - Return"
     Write-Host "`t1 - Enter the distinguished name"
     Write-Host "`t2 - Help"
-    Write-Host "============================================="
+    footer
     $choice = Read-Host -Prompt "What do you want to do?"
   }while($choice -lt 0 -or $choice -gt 3)
   return $choice;
 }
 
+# Restoration menu
 Function menuRestoration{
   $choice = 1
+  $min = 0
+  $max = 3
   do{
     cls #clean the screen
     if( $choice -lt 0 -or $choice -gt 3){
        mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter a value between 0 and 3. Enter '3' for help"
     }
-    Write-Host "`nDomain : $Global:DOMAIN"
-    Write-Host "User : $Global:USER`n"
-    Write-Host "============================================="
-    Write-Host "   RESTORATATION OF THE RIGHT ENVIRONMENT"
-    Write-Host "=============================================`n"
+    header("MENU : RESTORATATION OF THE RIGHT ENVIRONMENT")
     Write-Host "`t0 - Return"
     Write-Host "`t1 - Restoration complete"
     Write-Host "`t2 - Restoration from a point"
     Write-Host "`t3 - Help"
-    Write-Host "============================================="
+    footer
     $choice = Read-Host -Prompt "What do you want to do?"
   }while($choice -lt 0 -or $choice -gt 3)
   return $choice;
 }
 
-Function menuModifyRight{
+# Edit an organization unityt manu
+Function menuEdit{
   $choice = 1
+  $min = 0
+  $max = 3
   do{
     cls #clean the screen
-    if( $choice -lt 0 -or $choice -gt 6){
-       mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter a value between 0 and 3. Enter '3' for help"
+    if( $choice -lt $min -or $choice -gt $max){
+       mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter a value between $min and $max. Enter '$max' for help"
     }
-    Write-Host "`nDomain : $Global:DOMAIN"
-    Write-Host "User : $Global:USER`n"
-    Write-Host "============================================="
-    Write-Host "                  MO"
-    Write-Host "=============================================`n"
+    header("MENU : EDIT AN ORGANIZATION UNITY")
     Write-Host "`t0 - Return"
-    Write-Host "============================================="
+    Write-Host "`t1 - Modify an ACL"
+    Write-Host "`t2 - Add an ACL"
+    Write-Host "`t3 - Help"
+    footer
     $choice = Read-Host -Prompt "What do you want to do?"
-  }while($choice -lt 0 -or $choice -gt 6)
+  }while($choice -lt $min -or $choice -gt $max)
   return $choice;
 }
 
-Function help{
+#============================# ALL FUNCTIONS HELP #============================#
+
+Function helpMainMenu{
   $choice = 1
+  $min = 0
+  $max = 0
   do{
     cls #clean the screen
-    if( $choice -lt 1 -or $choice -gt 6){
-       mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter a value between 0 and 3. Enter '3' for help"
+    if( $choice -lt $min -or $choice -gt $max){
+       mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter '$max' for quit "
     }
-    Write-Host "`nDomain : $Global:DOMAIN"
-    Write-Host "User : $Global:USER`n"
-    Write-Host "============================================="
-    Write-Host "                  MENU"
-    Write-Host "=============================================`n"
-    Write-Host "============================================="
-    $choice = Read-Host -Prompt "What do you want to do?"
-  }while($choice -lt 1 -or $choice -gt 6)
+    header("HELP : MENU PRINCIPAL")
+    Write-Host "1 - Save the right environment`n"
+    Write-Host "`tMake a backup of all right of all Organizations Unities`n"
+    Write-Host "2 - Display the right environment`n"
+    Write-Host "`tDisplay all right of one Organizations Unities`n"
+    Write-Host "3 - Restoration of the right environment`n"
+    Write-Host "`tWith a backup, You can make a restoration"
+    Write-Host "`tof total or partial`n"
+    Write-Host "4 - Edit an organization unity`n"
+    Write-Host "`tAdd or Modify an ACE in ACL`n"
+    Write-Host "5 - Change security environment`n"
+    Write-Host "`tChange the environment with which you connected"
+    Write-Host "`tChange your domain name or your username `n"
+    Write-Host "7 - Quit`n"
+    Write-Host "`tQuit the program `n"
+    footer
+    $choice = Read-Host -Prompt "Enter 0 to quit "
+  }while($choice -lt $min -or $choice -gt $max)
+  return $choice;
+}
+
+Function helpBackupMenu{
+  $choice = 1
+  $min = 0
+  $max = 0
+  do{
+    cls #clean the screen
+    if( $choice -lt $min -or $choice -gt $max){
+       mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter '$max' for quit "
+    }
+    header("HELP : BACKUP")
+    Write-Host "0 - Return`n"
+    Write-Host "`tReturn at the main menu`n"
+    Write-Host "1 - Yes`n"
+    Write-Host "`tMake a backup of all right of all Organizations Unities`n"
+    Write-Host "2 - No`n"
+    Write-Host "`tCancel the backup`n"
+    footer
+    $choice = Read-Host -Prompt "Enter 0 to quit "
+  }while($choice -lt $min -or $choice -gt $max)
+  return $choice;
+}
+
+Function helpDisplayRightMenu{
+  $choice = 1
+  $min = 0
+  $max = 0
+  do{
+    cls #clean the screen
+    if( $choice -lt $min -or $choice -gt $max){
+       mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter '$max' for quit "
+    }
+    header("HELP : DISPLAY RIGHT MENU")
+    Write-Host "0 - Return`n"
+    Write-Host "`tReturn at the main menu`n"
+    Write-Host "1 - Enter the distinguished name`n"
+    Write-Host "`tEnter the distinguished name of your object"
+    Write-Host "`tfor display its right`n"
+    footer
+    $choice = Read-Host -Prompt "Enter 0 to quit "
+  }while($choice -lt $min -or $choice -gt $max)
+  return $choice;
+}
+
+Function helpRestorationMenu{
+  $choice = 1
+  $min = 0
+  $max = 0
+  do{
+    cls #clean the screen
+    if( $choice -lt $min -or $choice -gt $max){
+       mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter '$max' for quit "
+    }
+    header("HELP : RESTORATION")
+    Write-Host "0 - Return`n"
+    Write-Host "`tReturn at the main menu`n"
+    Write-Host "1 - Restoration complete`n"
+    Write-Host "`tMake a restoration complete with an backup file`n"
+    Write-Host "2 - Restoration from a point`n"
+    Write-Host "`tMake a restoration partial, start at on of organization"
+    Write-Host "`tunities, with an backup file`n"
+    footer
+    $choice = Read-Host -Prompt "Enter 0 to quit "
+  }while($choice -lt $min -or $choice -gt $max)
+  return $choice;
+}
+
+Function helpEditMenu{
+  $choice = 1
+  $min = 0
+  $max = 0
+  do{
+    cls #clean the screen
+    if( $choice -lt $min -or $choice -gt $max){
+       mess_error -error_mess "You enter a wrong value ($choice)" -help_mess "Please enter '$max' for quit "
+    }
+    header("HELP : EDIT MENU")
+    Write-Host "0 - Return`n"
+    Write-Host "`tReturn at the main menu`n"
+    Write-Host "1 - Modify an ACL"
+    Write-Host "`tYou can modify an ACL on an organization unities"
+    Write-Host "`tChange the right or who it applies to`n"
+    Write-Host "2 - Add an ACL"
+    Write-Host "Create an ACL on an organization unities`n"
+    footer
+    $choice = Read-Host -Prompt "Enter 0 to quit "
+  }while($choice -lt $min -or $choice -gt $max)
   return $choice;
 }
 
 
+#============================# FUNCTION BACKUP #============================#
 
 Function backup{
-$Date = Get-Date -UFormat "%Y_%m_%d_%H_%M"
+  $Date = Get-Date -UFormat "%Y_%m_%d_%H_%M"
+  $OutFile = "C:\Backup\Backup_$Date.csv"
 
-$OutFile = "C:\Backup\Backup_$Date.csv"
-
-
-if (Test-Path $OutFile){
+  if (Test-Path $OutFile){
     Del $OutFile
-}
+  }
 
-
-if (!(Test-Path -Path "C:\Backup")){
+  if (!(Test-Path -Path "C:\Backup")){
     New-Item -ItemType Directory -Path C:\Backup
+  }
 
-}
+  $InputDN = Read-Host -Prompt "Write the DistinguishedName of the Organisation Unit"
 
+  Import-Module ActiveDirectory
+  set-location ad:
+  (Get-Acl $InputDN).access | ft identityreference, accesscontroltype, isinherited -autosize
 
+  $Childs = Get-ChildItem $InputDN -recurse
 
-
-$InputDN = Read-Host -Prompt "Write the DistinguishedName of the Organisation Unit"
-
-Import-Module ActiveDirectory
-set-location ad:
-
-(Get-Acl $InputDN).access | ft identityreference, accesscontroltype, isinherited -autosize
-
-
-
-$Childs = Get-ChildItem $InputDN -recurse
-
-
-foreach($Child in $Childs){
-
-
+  foreach($Child in $Childs){
     Write-Host $Child.distinguishedName
-    
     $Header = $Child.distinguishedName
-    
     Add-Content -Value $Header -Path $OutFile
 
-    
     $Header = "IdentityReference,AccessControlType,IsInherited"
-    Add-Content -Value $Header -Path $OutFile 
-    
+    Add-Content -Value $Header -Path $OutFile
 
-
-    
-     
     (Get-Acl $Child.DistinguishedName).access | ft identityreference, accesscontroltype, isinherited -autosize
-    
-     $ACLs = Get-Acl $Child.DistinguishedName | ForEach-Object {$_.access}
 
-
-
+    $ACLs = Get-Acl $Child.DistinguishedName | ForEach-Object {$_.access}
 
     Foreach ($ACL in $ACLs){
-	    $OutInfo = $ACL.identityreference
-
-
-       if ($ACL.AccessControlType -eq "Allow"){
-            $OutInfo = "$OutInfo, Allow"
-
-        } else {
-            $OutInfo = "$OutInfo, Deny"
-        }
-
-
-        if ($ACL.IsInherited -eq "True"){
-            $OutInfo = "$OutInfo, True"
-
-        } else {
-            $OutInfo = "$OutInfo, False"
-        }
-        
-
-
-	    Add-Content -Value $OutInfo -Path $OutFile
-	}
-
-    
+      $OutInfo = $ACL.identityreference
+      if($ACL.AccessControlType -eq "Allow"){
+          $OutInfo = "$OutInfo, Allow"
+      }else {
+        $OutInfo = "$OutInfo, Deny"
+      }
+      if ($ACL.IsInherited -eq "True"){
+        $OutInfo = "$OutInfo, True"
+      }else{
+        $OutInfo = "$OutInfo, False"
+      }
+      Add-Content -Value $OutInfo -Path $OutFile
+    }
+  }
 }
 
 
-
-}
-
+#============================# MAIN #============================#
 
 TestUserCredentials
 
 $again = 1
+$againSubMenu = 1
+
 
 do{
   switch (menuMain){
   #1 - Save the right environment
     1{
-      switch(menuBackup){
-      #1 - Yes
-        1{
-          backup
+      do{
+        switch(menuBackup){
+        #1 - Yes
+          1{
+             backup
+          }
+        #2 - No
+          2{
+            $againSubMenu = 0
+          }
+        #3 - Help
+          3{
+            helpBackupMenu
+          }
+        #0 - Return
+          default{
+            $againSubMenu = 0
+          }
         }
-      #2 - No
-        2{
+      }while($againSubMenu -eq 1)
 
-        }
-      #3 - Help
-        3{
-
-        }
-      #0 - Return
-        default{}
-      }
     }
   #2 - Display the right environment
     2{
+    do{
       switch(menuDisplayRight){
       #1 - Enter the distinguished name
         1{
@@ -394,42 +482,70 @@ do{
         }
       #2 - Help
         2{
-
+          helpDisplayRightMenu
         }
       #0 - Return
-        default{}
+        default{
+          $againSubMenu = 0
+        }
       }
+    }while($againSubMenu -eq 1)
+
     }
   #3 - Restoration of the right environment
     3{
-      switch(menuRestoration){
-      #1 - Restoration complete
-        1{
+      do{
+        switch(menuRestoration){
+        #1 - Restoration complete
+          1{
 
-        }
-      #2 - Restoration from a point
-        2{
+          }
+        #2 - Restoration from a point
+          2{
 
+          }
+        #3 - Help
+          3{
+            helpRestorationMenu
+          }
+        #0 - Return
+          default{
+            $againSubMenu = 0
+          }
         }
-      #3 - Help
-        3{
-
-        }
-      #0 - Return
-        default{}
-      }
+      }while($againSubMenu -eq 1)
     }
   #4 - Modify the right environment
     4{
+      do{
+        switch(menuEdit){
+        #1 - Modify an ACL
+          1{
+
+          }
+        #2 - Add an ACL
+          2{
+
+          }
+        #3 - Help
+          3{
+
+          }
+        #0 - Return
+          default{
+            $againSubMenu
+          }
+        }
+      }while($againSubMenu -eq 1)
 
     }
   #5 - Change security environment
     5{
-        TestUserCredentials
+      TestUserCredentials
     }
   #6 - Help
     6{
-
+      helpMainMenu
     }
   #7 - Quit
     7{
@@ -442,4 +558,4 @@ do{
 }while($again -eq 1)
 
 
-Write-Host " Good Bye !"
+Write-Host " Good Bye $Global:USER!"
