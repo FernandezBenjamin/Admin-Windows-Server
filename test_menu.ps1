c9699b76d3e751a4f255640f640c49b79235719a
Quentin <boulanger.quentinpro@gmail.com>
2018-01-08 23:38:01 +0100

    Ca fait de la merde

--- test_menu.ps1
+++ test_menu.ps1
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿
 #Variable globale
 $Local:domainName = $null
@@ -33,3 +34,29 @@ Write-Host "============================================="
 menu
 
 $Main_menu_choice = Read-Host -Prompt "What do you want to do?"
+=======
+﻿#Allow the script execution
+Set-ExecutionPolicy Unrestricted
+
+$Domain = Read-Host -Prompt "Please input your domain name"
+$Username = Read-Host -Prompt "Please input your username"
+
+$User = Get-Credential -Credential $Username
+
+Write-Host "Welcome"$User.UserName
+
+function menu
+{
+    Write-Host "1 - Save the security environment"
+    Write-Host "2 - Display the security environment"
+    Write-Host "2 - Restoration of the security environment"
+    Write-Host "3 - Modify the security environment"
+    Write-Host "4 - Change authentification"
+    Write-Host "5 - Help"
+    Write-Host "6 - Quit"
+}
+cls
+menu
+
+$Main_menu_choice = Read-Host -Prompt "What do you want to do?"
+>>>>>>> Stashed changes
