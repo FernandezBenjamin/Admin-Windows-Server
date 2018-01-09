061c3a53ef0f6d5ed085d0ea521d0c5c449fff98
Fernandez Benjamin <fernandez.benjamin@outlook.com>
2018-01-09 13:05:12 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -396,51 +396,96 @@ Function helpEditMenu{
 
 Function backup{
   $Date = Get-Date -UFormat "%Y_%m_%d_%H_%M"
-  $OutFile = "C:\Backup\Backup_$Date.csv"
 
-  if (Test-Path $OutFile){
+$OutFile = "C:\Backup\Backup_$Date.csv"
+
+
+if (Test-Path $OutFile){
     Del $OutFile
-  }
+}
 
-  if (!(Test-Path -Path "C:\Backup")){
+
+if (!(Test-Path -Path "C:\Backup")){
     New-Item -ItemType Directory -Path C:\Backup
-  }
 
-  $InputDN = Read-Host -Prompt "Write the DistinguishedName of the Organisation Unit"
+}
 
-  Import-Module ActiveDirectory
-  set-location ad:
-  (Get-Acl $InputDN).access | ft identityreference, accesscontroltype, isinherited -autosize
 
-  $Childs = Get-ChildItem $InputDN -recurse
 
-  foreach($Child in $Childs){
+$Header = "IdentityReference,AccessControlType,IsInherited,PropagationFlags"
+Add-Content -Value $Header -Path $OutFile 
+
+
+$InputDN = Read-Host -Prompt "Write the DistinguishedName of the Organisation Unit"
+
+Import-Module ActiveDirectory
+set-location ad:
+
+(Get-Acl $InputDN).access | ft identityreference, accesscontroltype, isinherited, propagantionflags -autosize
+
+
+
+$Childs = Get-ChildItem $InputDN -recurse
+
+
+foreach($Child in $Childs){
+
+
+    
+
     Write-Host $Child.distinguishedName
+    
     $Header = $Child.distinguishedName
+
+    $header = $header -replace ',','/'
+    
     Add-Content -Value $Header -Path $OutFile
+    
 
-    $Header = "IdentityReference,AccessControlType,IsInherited"
-    Add-Content -Value $Header -Path $OutFile
 
-    (Get-Acl $Child.DistinguishedName).access | ft identityreference, accesscontroltype, isinherited -autosize
+    
+     
+    (Get-Acl $Child.DistinguishedName).access | ft identityreference, accesscontroltype, isinherited, propagationflags -autosize
+    
+     $ACLs = Get-Acl $Child.DistinguishedName | ForEach-Object {$_.access}
 
-    $ACLs = Get-Acl $Child.DistinguishedName | ForEach-Object {$_.access}
+
+
 
     Foreach ($ACL in $ACLs){
-      $OutInfo = $ACL.identityreference
-      if($ACL.AccessControlType -eq "Allow"){
-          $OutInfo = "$OutInfo, Allow"
-      }else {
-        $OutInfo = "$OutInfo, Deny"
-      }
-      if ($ACL.IsInherited -eq "True"){
-        $OutInfo = "$OutInfo, True"
-      }else{
-        $OutInfo = "$OutInfo, False"
-      }
-      Add-Content -Value $OutInfo -Path $OutFile
-    }
-  }
+	    $OutInfo = $ACL.identityreference
+
+
+       if ($ACL.AccessControlType -eq "Allow"){
+            $OutInfo = "$OutInfo, Allow"
+
+        } else {
+            $OutInfo = "$OutInfo, Deny"
+        }
+
+
+        if ($ACL.IsInherited -eq "True"){
+            $OutInfo = "$OutInfo, True"
+
+        } else {
+            $OutInfo = "$OutInfo, False"
+        }
+        
+
+        if ($ACL.PropagationFlags -eq "InheritOnly"){
+            $OutInfo = "$OutInfo, InheritOnly"
+        } else {
+            $OutInfo = "$OutInfo, None"
+
+        }
+
+
+	    Add-Content -Value $OutInfo -Path $OutFile
+	}
+
+    
+}
+
 }
 
 Function Get-ADSIOU
